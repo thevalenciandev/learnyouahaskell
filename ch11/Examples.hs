9c1f9920907df775f90f3c47f885aa285d7c7072
TVD <thevalenciandev@gmail.com>
2021-05-16 19:07:08 +0100

    More Applicative examples

--- ch11/Examples.hs
+++ ch11/Examples.hs
@@ -25,3 +25,15 @@ ex1 :: Maybe Integer
 ex1 = Just (*3) <*> Just 3
 ex2 :: Maybe Integer
 ex2 = pure (*3) <*> Just 3
+ex3 :: Maybe String
+ex3 = pure (++"YOLO") <*> Just "YO, "
+ex4 = Nothing <*> Just "Beurk"
+-- Note that all those could have been implemented simply with fmap
+ex5 = fmap (*3) (Just 3)
+-- But applicatives get useful when applying a fn over multiple Functors
+-- They're left-associative
+ex6 = pure (+) <*> Just 2 <*> Just 40
+ex7 = pure (++) <*> Just "Ha" <*> Just "Ha"
+ex8 = (++) <$> Just "johntra" <*> Just "volta"
+-- The above is the equivalent to the following non-applicative example
+ex9 = (++) "johntra" "volta"
